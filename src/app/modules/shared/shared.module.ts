72cec3669cf998bd6b1ac158e3b9589e7ec6c569
Andres Garcia <andres.fgp@hotmail.com>
2021-11-03 15:34:32 -0500

    headerAndFooter

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -5,7 +5,7 @@ import { ExponentialPipe } from './pipes/exponential/exponential.pipe';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
-
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [
@@ -20,7 +20,8 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MaterialModule
   ],
 })
 export class SharedModule { }
